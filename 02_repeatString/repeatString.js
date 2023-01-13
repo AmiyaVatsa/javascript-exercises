87b7cf9817581619c4217b6e0380d9935f3640e2
Amiya Vatsa <amiyavatsa1807@gmail.com>
2023-01-13 22:39:20 +0530

    Did 1 and 2 questions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(name, num) {
+    text = "";
+    for (let i=0; i<num; i++)
+    {
+        text += name;
+    }
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    else{return text;}
+    
 };
 
 // Do not edit below this line
