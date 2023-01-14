c73896820768b10a9e2c96e35ce8c3cf49d842f4
Amiya Vatsa <amiyavatsa1807@gmail.com>
2023-01-14 11:45:04 +0530

    completed problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (typeof a === 'string' || typeof b === 'string' || typeof a === 'object' || typeof b === 'object')
+    {
+        return "ERROR";
+    }
+    if ( a < 0 || b < 0 )
+    {
+        return "ERROR";
+    }
+    let sum = 0;
+    let m = Math.max(a , b);
+    let n = Math.min(a , b);
+    for (let i = n; i <= m; i++)
+    {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
