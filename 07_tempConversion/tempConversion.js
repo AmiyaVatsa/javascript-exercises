b9df1dfa36c33cbbf79db2d8feb33b4b0fab57cc
Amiya Vatsa <amiyavatsa1807@gmail.com>
2023-01-14 12:52:40 +0530

    completed all exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faeh) {
+  const cel = Math.round(((faeh - 32) / 1.8)*10)/10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const faeh = Math.round((cel*1.8 + 32) * 10)/10;
+  return faeh;
 };
 
 // Do not edit below this line
